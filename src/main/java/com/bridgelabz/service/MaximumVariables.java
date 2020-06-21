c4cbdb0f957441c01f793652d2e03d10692741a4
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-21 21:58:17 +0530

    ADDED- Class Crated And Welcome Message

---
+++ src/main/java/com/bridgelabz/service/MaximumVariables.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.service;
+
+public class MaximumVariables {
+    public static void main(String[] args){
+        System.out.println("Find Maximum Problem Using Generics");
+    }
+}
---
+++ src/test/java/com/bridgelabz/test/MaximumVariablesTest.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.test;
+
+public class MaximumVariablesTest {
+    public static void main(String[] args){
+        System.out.println("Find Maximum Problem Using Generics");
+    }
+}
