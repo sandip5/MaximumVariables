831bb3d88d1d3930b419c4e6de8e0dff5612ed76
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-22 00:14:42 +0530

    ADDED- Float Variable At First, Second, Third Position Return Same Variable

--- src/test/java/com/bridgelabz/test/MaximumVariablesTest.java
+++ src/test/java/com/bridgelabz/test/MaximumVariablesTest.java
@@ -7,18 +7,33 @@ import org.junit.Test;
 public class MaximumVariablesTest {
     MaximumVariables maximumVariablesObject =new MaximumVariables();
     @Test
-    public void givenNumber_AtFirstPosition_WhenMaximum_ShouldReturnTrue() {
+    public void givenIntegerVariable_AtFirstPosition_WhenMaximum_ShouldReturnTrue() {
         Integer[] variables = {123, 63, 92};
         Assert.assertEquals(maximumVariablesObject.searchMaximum(variables), variables[0]);
     }
     @Test
-    public void givenNumber_AtSecondPosition_WhenMaximum_ShouldReturnTrue() {
+    public void givenIntegerVariable_AtSecondPosition_WhenMaximum_ShouldReturnTrue() {
         Integer[] variables = {123, 180, 92};
         Assert.assertEquals(maximumVariablesObject.searchMaximum(variables), variables[1]);
     }
     @Test
-    public void givenNumber_AtThirdPosition_WhenMaximum_ShouldReturnTrue() {
+    public void givenIntegerVariable_AtThirdPosition_WhenMaximum_ShouldReturnTrue() {
         Integer[] variables = {123, 180, 921};
+        Assert.assertEquals(maximumVariablesObject.searchMaximum(variables), variables[2]);
+    }
+    @Test
+    public void givenFloatVariable_AtFirstPosition_WhenMaximum_ShouldReturnTrue() {
+        Float[] variables = {123.5f, 63.2f, 92.0f};
+        Assert.assertEquals(maximumVariablesObject.searchMaximum(variables), variables[0]);
+    }
+    @Test
+    public void givenFloatVariable_AtSecondPosition_WhenMaximum_ShouldReturnTrue() {
+        Float[] variables = {123.5f, 635.2f, 92.0f};
+        Assert.assertEquals(maximumVariablesObject.searchMaximum(variables), variables[1]);
+    }
+    @Test
+    public void givenFloatVariable_AtThirdPosition_WhenMaximum_ShouldReturnTrue() {
+        Float[] variables = {123.5f, 63.2f, 921.034f};
         Assert.assertEquals(maximumVariablesObject.searchMaximum(variables), variables[2]);
     }
 }
