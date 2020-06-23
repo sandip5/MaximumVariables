88c1381c94ccef87faf58264b6add402063225b3
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-23 10:07:24 +0530

    REFACTORED- Changes In Code Declared Gloabal Object and Convert Static To Default

--- src/main/java/com/bridgelabz/service/MaximumVariables.java
+++ src/main/java/com/bridgelabz/service/MaximumVariables.java
@@ -2,16 +2,17 @@ package com.bridgelabz.service;
 
 import java.util.Arrays;
 
-public class MaximumVariables<T extends Comparable<T>> {
-    public static <T extends Comparable<T>> T searchMaximum(T... elements) {
-        T[] variables= elements;
+@SuppressWarnings("ALL")
+public class MaximumVariables<T> {
+    public T searchMaximum(T... elements) {
+        T[] variables = elements;
         int length = variables.length;
         Arrays.sort(variables);
         print(variables);
-        System.out.println(variables[length-1]);
+        System.out.println(variables[length - 1]);
         return variables[length - 1];
     }
-    public static <T> void print(T[] inputArray){
+    public void print(T[] inputArray) {
         for (T element : inputArray)
             System.out.printf("%s ", element);
         System.out.println();
--- src/test/java/com/bridgelabz/test/MaximumVariablesTest.java
+++ src/test/java/com/bridgelabz/test/MaximumVariablesTest.java
@@ -4,54 +4,55 @@ import com.bridgelabz.service.MaximumVariables;
 import org.junit.Assert;
 import org.junit.Test;
 
+@SuppressWarnings("ALL")
 public class MaximumVariablesTest {
+    MaximumVariables maximum=new MaximumVariables();
     @Test
     public void givenIntegerVariable_AtFirstPosition_WhenMaximum_ShouldReturnTrue() {
-        Assert.assertEquals(123, (Comparable)MaximumVariables.searchMaximum(123, 63, 92));
+        Assert.assertEquals(123, maximum.searchMaximum(123, 63, 92));
     }
     @Test
     public void givenIntegerVariable_AtSecondPosition_WhenMaximum_ShouldReturnTrue() {
-        Assert.assertEquals(180, (Comparable)MaximumVariables.searchMaximum(123, 180, 92));
+        Assert.assertEquals(180, maximum.searchMaximum(123, 180, 92));
     }
     @Test
     public void givenIntegerVariable_AtThirdPosition_WhenMaximum_ShouldReturnTrue() {
-        Assert.assertEquals(921, (Comparable)MaximumVariables.searchMaximum(123, 180, 921));
+        Assert.assertEquals(921, maximum.searchMaximum(123, 180, 921));
     }
     @Test
     public void givenMoreThanThreeIntegerVariable_WhenProper_ShouldReturnTrue() {
-        Assert.assertEquals(1230, (Comparable)MaximumVariables.searchMaximum(123, 180, 921,1230,56));
+        Assert.assertEquals(1230, maximum.searchMaximum(123, 180, 921,1230,56));
     }
     @Test
     public void givenFloatVariable_AtFirstPosition_WhenMaximum_ShouldReturnTrue() {
-        Assert.assertEquals(123.5f, (Comparable)MaximumVariables.searchMaximum(123.5f, 63.2f, 92.0f));
+        Assert.assertEquals(123.5f, maximum.searchMaximum(123.5f, 63.2f, 92.0f));
     }
     @Test
     public void givenFloatVariable_AtSecondPosition_WhenMaximum_ShouldReturnTrue() {
-        Comparable maximum = MaximumVariables.searchMaximum(123.5f, 635.2f, 92.0f);
-        Assert.assertEquals(635.2f, (Comparable)MaximumVariables.searchMaximum(123.5f, 635.2f, 92.0f));
+        Assert.assertEquals(635.2f, maximum.searchMaximum(123.5f, 635.2f, 92.0f));
     }
     @Test
     public void givenFloatVariable_AtThirdPosition_WhenMaximum_ShouldReturnTrue() {
-        Assert.assertEquals(921.034f, (Comparable)MaximumVariables.searchMaximum(123.5f, 63.2f, 921.034f));
+        Assert.assertEquals(921.034f, maximum.searchMaximum(123.5f, 63.2f, 921.034f));
     }
     @Test
     public void givenMoreThanThreeFloatVariable_WhenProper_ShouldReturnTrue() {
-        Assert.assertEquals(921.034f, (Comparable)MaximumVariables.searchMaximum(123.5f, 63.2f, 921.034f,646.45f,894.00045f));
+        Assert.assertEquals(921.034f, maximum.searchMaximum(123.5f, 63.2f, 921.034f,646.45f,894.00045f));
     }
     @Test
     public void givenStringVariable_AtFirstPosition_WhenMaximum_ShouldReturnTrue() {
-        Assert.assertEquals("Peach", (Comparable)MaximumVariables.searchMaximum("Peach", "Apple", "Banana"));
+        Assert.assertEquals("Peach", maximum.searchMaximum("Peach", "Apple", "Banana"));
     }
     @Test
     public void givenStringVariable_AtSecondPosition_WhenMaximum_ShouldReturnTrue() {
-        Assert.assertEquals("Peach", (Comparable)MaximumVariables.searchMaximum("Apple", "Peach", "Banana"));
+        Assert.assertEquals("Peach", maximum.searchMaximum("Apple", "Peach", "Banana"));
     }
     @Test
     public void givenStringVariable_AtThirdPosition_WhenMaximum_ShouldReturnTrue() {
-        Assert.assertEquals("Peach", (Comparable)MaximumVariables.searchMaximum("Apple", "Banana", "Peach"));
+        Assert.assertEquals("Peach", maximum.searchMaximum("Apple", "Banana", "Peach"));
     }
     @Test
     public void givenMoreThanThreeStringVariable_WhenProper_ShouldReturnTrue() {
-        Assert.assertEquals("Peach", (Comparable)MaximumVariables.searchMaximum("Apple", "Banana", "Peach","Go"));
+        Assert.assertEquals("Peach", maximum.searchMaximum("Apple", "Banana", "Peach","Go"));
     }
 }
